f35334a7d029204929fb2ea753084823282e4136
rocketdv <leviu02@gmail.com>
2020-06-29 12:34:22 -0400

    init

--- README.md
+++ README.md
@@ -1,22 +1,20 @@
-# header-format README
+# Header Format
 
-This is the README for your extension "header-format". After writing up a brief description, we recommend including the following sections.
+This extension allows you to easily paste and format headers from Charles or Dev tools as a json object, current supports only JS, plan on support more languages in the future, pull requests are welcome!
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+Open up the command palette (`Ctrl or Cmd + Shift + P`) and type format headers, select either from selection or from clipboard
 
-For example if there is an image subfolder under your extension project workspace:
+<!-- \!\[feature X\]\(images/feature-x.png\) -->
 
-\!\[feature X\]\(images/feature-x.png\)
+<!-- > Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow. -->
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+<!-- ## Requirements
 
-## Requirements
+If you have any requirements or dependencies, add a section describing those and how to install and configure them. -->
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
-
-## Extension Settings
+<!-- ## Extension Settings
 
 Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
 
@@ -29,37 +27,13 @@ This extension contributes the following settings:
 
 ## Known Issues
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+Calling out known issues can help limit users opening duplicate issues against your extension. -->
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
+Created the commands for extension
 
 ### 1.0.0
 
-Initial release of ...
+Initial release
 
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
--- extension.js
+++ extension.js
@@ -5,10 +5,30 @@ const vscode = require('vscode');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
+function getFormatted(clip) {
+	return clip
+		.split('\n')
+		.map(x => x.trim())
+		.map(x => /^(.[^\s|\t|:]+)[\s|: |:|\t](.*)$/g.exec(x))
+		.filter(x => x)
+		.map(x => {
+			try {
+				let [_, k, v] = x
+				v = v.trim()
+				k = /^\d|-/.test(k) ? `'${k}'` : k
+				v = /^[0-9]+$/.test(v) ? v : `'${v}'`
+
+				return [k, v].join(': ')
+			} catch (e) { }
+		})
+		.filter(x => x)
+		.join(',\n')
+}
+
 /**
  * @param {vscode.ExtensionContext} context
  */
-async function activate(context) {
+function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -17,11 +37,19 @@ async function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let pasteFormatCmd = vscode.commands.registerTextEditorCommand('extension.format_from_clipboard', editor => {
+	let pasteFormatCmd = vscode.commands.registerCommand('extension.format_from_clipboard', async () => {
 		// vscode.window.showInformationMessage('Hello World!');
-		let clipboard = await vscode.env.clipboard.readText();
+		const editor = vscode.window.activeTextEditor
+		if (!editor) { return vscode.window.showErrorMessage('No active editor selected!') }
+		let clipboard = await vscode.env.clipboard.readText() || '';
 		// const text = result.lines.join("\n");
-		let result = clipboard
+		let result
+		try {
+			result = getFormatted(clipboard)
+		} catch (e) {
+			console.log(e)
+			return vscode.window.showErrorMessage('Error parsing headers!')
+		}
 		const selection = editor.selection;
 		editor.edit(builder => {
 			if (selection.isEmpty) {
@@ -33,7 +61,32 @@ async function activate(context) {
 		});
 	});
 
-	context.subscriptions.push(pasteFormatCmd);
+	let selectFormatCmd = vscode.commands.registerCommand('extension.format_from_selection', async () => {
+		// vscode.window.showInformationMessage('Hello World!');
+		const editor = vscode.window.activeTextEditor
+		if (!editor) { return vscode.window.showErrorMessage('No active editor selected!') }
+		const selection = editor.selection
+		let raw = editor.document.getText(selection);
+		// const text = result.lines.join("\n");
+		let result
+		try {
+			result = getFormatted(raw)
+		} catch (e) {
+			console.log(e)
+			return vscode.window.showErrorMessage('Error parsing headers!')
+		}
+
+		editor.edit(builder => {
+			if (selection.isEmpty) {
+				builder.insert(selection.start, result);
+			}
+			else {
+				builder.replace(new vscode.Range(selection.start, selection.end), result);
+			}
+		});
+	});
+
+	context.subscriptions.push(pasteFormatCmd, selectFormatCmd);
 }
 exports.activate = activate;
 
